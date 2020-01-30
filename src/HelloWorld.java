a7f4bb04edff46c40f2a7083e38887a143d9a645
Shee <Suman.Shee@Honeywell.com>
2020-01-30 21:54:15 +0530

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+	    
+		int i;
+		int j;
+		for(i=0;i<5;i++)
+			
+			
+		{
+			for(j=0;i<i;i++)
+			{
+			
+			System.out.println("/n");
+			}
+			System.out.println("lol");
+		}
+			
+		
+		
+	  }
+
+}
